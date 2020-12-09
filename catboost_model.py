c82bd9106ca21b9fe9c69dd5a4d9c669f3bea821
akshit-agarwal-123 <akshit@email.arizona.edu>
2020-12-08 22:15:39 -0700

    feat: baseline catboost model

--- catboost_model.py
+++ catboost_model.py
@@ -1,5 +1,5 @@
 from catboost import CatBoostClassifier
-from model import import_data, clean_data, one_hot_encode, split_dataset, set_df_values
+from model import import_data, clean_data, one_hot_encode, split_dataset, set_df_values, make_predictions, get_scores
 from sklearn.multiclass import OneVsRestClassifier
 
 
@@ -15,7 +15,6 @@ def fit_model(x_train, y_train, x_test, y_test):
 
     ovr = OneVsRestClassifier(estimator=CatBoostClassifier(iterations=10, random_state=1))
     ovr.fit(x_train, y_train)
-
 
     # model_cb = CatBoostClassifier(iterations=100,
     #                               learning_rate=0.02,
@@ -46,5 +45,13 @@ if __name__ == '__main__':
 
     model = fit_model(x_train, y_train, x_val, y_val)
 
-    
+    h1n1_preds, seasonal_preds = make_predictions(model, x_train)
+    h1n1_true, seasonal_true = y_train['h1n1_vaccine'].values.tolist(), y_train['seasonal_vaccine'].values.tolist()
+    score = get_scores(h1n1_true, h1n1_preds, seasonal_true, seasonal_preds)
+    print(f'Training Accuracy: {score}')
+
+    h1n1_preds, seasonal_preds = make_predictions(model, x_val)
+    h1n1_true, seasonal_true = y_val['h1n1_vaccine'].values.tolist(), y_val['seasonal_vaccine'].values.tolist()
+    score = get_scores(h1n1_true, h1n1_preds, seasonal_true, seasonal_preds)
+    print(f'Validation Accuracy: {score}')
     print('done')
