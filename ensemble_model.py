b875d9a41c8c1c7e716863bbd046c7729d005f52
akshit-agarwal-123 <akshit@email.arizona.edu>
2020-12-12 04:29:29 -0700

    feat: randomforest and gaussianNB

--- ensemble_model.py
+++ ensemble_model.py
@@ -1,12 +1,9 @@
 import numpy as np
-from mlxtend.classifier import StackingClassifier
 from model import import_data, clean_data, split_dataset, set_df_values, one_hot_encode, make_predictions, get_scores
 from pandas import to_numeric
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
 
 if __name__ == '__main__':
     df = import_data(train=True)
@@ -23,9 +20,9 @@ if __name__ == '__main__':
     X_train, Y_train = np.array(x_train), np.array(y_train)
     X_val, Y_val = np.array(x_val), np.array(y_val)
 
-    clf1 = OneVsRestClassifier(KNeighborsClassifier(n_neighbors=1))
-    # clf2 = OneVsRestClassifier(RandomForestClassifier(random_state=1, verbose=2))
-    # clf3 = OneVsRestClassifier(GaussianNB())
+    # clf1 = OneVsRestClassifier(KNeighborsClassifier(n_neighbors=1)) # 65%
+    # clf1 = OneVsRestClassifier(RandomForestClassifier(random_state=1, verbose=2))  # 83%
+    clf1 = OneVsRestClassifier(GaussianNB())  #74%
     # lr = OneVsRestClassifier(LogisticRegression(verbose=2))
     # sclf = StackingClassifier(classifiers=[clf1, clf2, clf3],
     #                           use_probas=True,
@@ -35,7 +32,6 @@ if __name__ == '__main__':
     y_train['seasonal_vaccine'] = to_numeric(y_train['seasonal_vaccine'])
 
     clf1.fit(x_train, y_train)
-
 
     h1n1_preds, seasonal_preds = make_predictions(clf1, X_val)
     h1n1_true, seasonal_true = (Y_val[:, 0]).tolist(), Y_val[:, 1].tolist()
