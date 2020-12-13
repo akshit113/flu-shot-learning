bc49274012a97db3fe1eb2e0f2cd224c0ddc1794
akshit-agarwal-123 <akshit@email.arizona.edu>
2020-12-13 16:19:27 -0700

    feat: baseline neural net

--- nn.py
+++ nn.py
@@ -3,6 +3,7 @@ from keras import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 from model import import_data, clean_data, split_dataset, set_df_values, one_hot_encode
+from sklearn.metrics import roc_auc_score
 
 
 def get_model(input_size, output_size, magic='tanh'):
@@ -54,6 +55,33 @@ def fit_and_evaluate(model, x_train, y_train, x_test, y_test, batch_size, epochs
     return test_acc, test_loss
 
 
+def make_predictions(model, x_test):
+    """This function makes predictions using the model on the unseen test dataset
+    :param y_test: test labels
+    :param model: Sequential model
+    :param x_test: unseen test dataset
+    :return: predictions in the binary numpy array format
+    """
+    predictions = model.predict(x_test)
+    # labels = (np.where(predictions < 0.5, 0, 1)).flatten()
+    if isinstance(predictions, list):
+        h1n1_preds = predictions[0]
+        seasonal_preds = predictions[1]
+
+    else:
+        h1n1_preds = predictions[:, 0].tolist()
+        seasonal_preds = predictions[:, 1].tolist()
+
+    return h1n1_preds, seasonal_preds
+
+
+def get_scores(h1n1_true, h1n1_preds, seasonal_true, seasonal_preds):
+    h1n1_score = roc_auc_score(np.array(h1n1_true), np.array(h1n1_preds))
+    seasonal_score = roc_auc_score(np.array(seasonal_true), np.array(seasonal_preds))
+    average_score = (h1n1_score + seasonal_score) / 2
+    return average_score
+
+
 if __name__ == '__main__':
     df = import_data(train=True)
     test_df = import_data(features='Datasets/test_set_features.csv', train=False)
@@ -75,6 +103,16 @@ if __name__ == '__main__':
     x_val = np.asarray(x_val).astype(np.float32)
     y_val = np.asarray(y_val).astype(np.float32)
 
-    test_acc, test_loss = fit_and_evaluate(model, x_train, y_train, x_val, y_val, batch_size=64, epochs=100)
-    print('test')
+    test_acc, test_loss = fit_and_evaluate(model, x_train, y_train, x_val, y_val, batch_size=64, epochs=1)
 
+    h1n1_preds, seasonal_preds = make_predictions(model, x_train)
+    h1n1_true, seasonal_true = y_train[:, 0].tolist(), y_train[:, 1].tolist()
+    train_score = get_scores(h1n1_true, h1n1_preds, seasonal_true, seasonal_preds)
+    print(f'Training Accuracy: {train_score}')
+
+    h1n1_preds, seasonal_preds = make_predictions(model, x_val)
+    h1n1_true, seasonal_true = y_val[:, 0].tolist(), y_val[:, 1].tolist()
+    validation_score = get_scores(h1n1_true, h1n1_preds, seasonal_true, seasonal_preds)
+    print(f'Validation Accuracy: {validation_score}')
+
+    print('program execution complete')
